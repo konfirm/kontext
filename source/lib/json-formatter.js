95cc68144fd6946b344a89051dd856117668e178
Rogier Spieker <rogier@konfirm.net>
2015-10-16 19:20:26 +0200

    Integrated JSONFormatted (hard, not the github repo version) and added its unit tests

--- source/lib/json-formatter.js
+++ source/lib/json-formatter.js
@@ -1,254 +1,238 @@
+'use strict';
+
 /**
  *  Format a string containing (valid) js variables into proper JSON so it can be handled by JSON.parse
- *  @package    JSONFormatter
- *  @author     Rogier Spieker <rogier@konfirm.eu>
+ *  @name       JSONFormatter
+ *  @package    Kontext
  */
-/*global define*/
-(function(scope, factory) {
-	'use strict';
-
-	// $lab:coverage:off$
-	var name = 'JSONFormatter';
-
-	if (typeof module === 'object' && typeof module.exports === 'object') {
-		module.exports = factory();
-	}
-	else if (typeof define === 'function' && define.amd) {
-		define(name, factory);
-	}
-	else {
-		(typeof exports === 'object' ? exports : scope)[name] = factory();
+function JSONFormatter() {
+	//  Implement a Singleton pattern and allow JSONFormatter to be invoked without the `new` keyword
+	if (typeof JSONFormatter.prototype.__instance !== 'undefined' || !(this instanceof JSONFormatter)) {
+		return JSONFormatter.prototype.__instance || new JSONFormatter();
 	}
 
-	// $lab:coverage:on$
+	//  Maintain a reference to the first instance (which - if exists - is returned in the flow above)
+	JSONFormatter.prototype.__instance = this;
 
-})(this, function() {
-	'use strict';
+	var formatter = this,
+		special = '\'":,{}[] ',
+		quotation = '"',
+		pattern = {
+			escape: /["\\\/\b\f\n\r\t]/,
+			noquote: /^(?:true|false|null|-?[0-9]+(?:\.[0-9]+)?)$/i,
+			trailer: /[,]+$/
+		};
 
-	function JSONFormatter() {
-		//  Implement a Singleton pattern and allow JSONFormatter to be invoked without the `new` keyword
-		if (typeof JSONFormatter.prototype.__instance !== 'undefined' || !(this instanceof JSONFormatter)) {
-			return JSONFormatter.prototype.__instance || new JSONFormatter();
+	/**
+	 *  Determine is a token is a special character
+	 *  @name    isSpecial
+	 *  @access  internal
+	 *  @param   string  token
+	 *  @return  bool  special
+	 */
+	function isSpecial(token) {
+		return special.indexOf(token) >= 0;
+	}
+
+	/**
+	 *  Add quotes if required
+	 *  @name    addQuotation
+	 *  @access  internal
+	 *  @param   string  token
+	 *  @param   bool    force
+	 *  @return  string  JSON-token
+	 */
+	function addQuotation(token, force) {
+		var quote = quotation;
+
+		//  if quotation is not enforced, we must skip application of quotes for certain tokens
+		if (!force && (isSpecial(token) || pattern.noquote.test(token))) {
+			quote = '';
 		}
 
-		//  Maintain a reference to the first instance (which - if exists - is returned in the flow above)
-		JSONFormatter.prototype.__instance = this;
+		return quote + token + quote;
+	}
 
-		var formatter = this,
-			special = '\'":,{}[] ',
-			quotation = '"',
-			pattern = {
-				escape: /["\\\/\b\f\n\r\t]/,
-				noquote: /^(?:true|false|null|-?[0-9]+(?:\.[0-9]+)?)$/i,
-				trailer: /[,]+$/
-			};
+	/**
+	 *  Remove trailing commas from the result stack
+	 *  @name    removeTrailing
+	 *  @access  internal
+	 *  @param   Array  result
+	 *  @return  Array  result
+	 */
+	function removeTrailing(result) {
+		return pattern.trailer.test(result) ? removeTrailing(result.substr(0, result.length - 1)) : result;
+	}
 
-		/**
-		 *  Determine is a token is a special character
-		 *  @name    isSpecial
-		 *  @access  internal
-		 *  @param   string  token
-		 *  @return  bool  special
-		 */
-		function isSpecial(token) {
-			return special.indexOf(token) >= 0;
-		}
+	/**
+	 *  Handle a quoted string, ensuring proper escaping for double quoted strings
+	 *  @name    escapeQuotedInput
+	 *  @access  internal
+	 *  @param   string  token
+	 *  @array   Array   list
+	 *  @return  Array   result
+	 */
+	function escapeQuotedInput(token, list) {
+		var result = [],
+			character;
 
-		/**
-		 *  Add quotes if required
-		 *  @name    addQuotation
-		 *  @access  internal
-		 *  @param   string  token
-		 *  @param   bool    force
-		 *  @return  string  JSON-token
-		 */
-		function addQuotation(token, force) {
-			var quote = quotation;
-
-			//  if quotation is not enforced, we must skip application of quotes for certain tokens
-			if (!force && (isSpecial(token) || pattern.noquote.test(token))) {
-				quote = '';
-			}
-
-			return quote + token + quote;
-		}
-
-		/**
-		 *  Remove trailing commas from the result stack
-		 *  @name    removeTrailing
-		 *  @access  internal
-		 *  @param   Array  result
-		 *  @return  Array  result
-		 */
-		function removeTrailing(result) {
-			return pattern.trailer.test(result) ? removeTrailing(result.substr(0, result.length - 1)) : result;
-		}
-
-		/**
-		 *  Handle a quoted string, ensuring proper escaping for double quoted strings
-		 *  @name    escapeQuotedInput
-		 *  @access  internal
-		 *  @param   string  token
-		 *  @array   Array   list
-		 *  @return  Array   result
-		 */
-		function escapeQuotedInput(token, list) {
-			var result = [],
-				character;
-
-			//  token is the initial (opening) quotation character, we are not (yet) interested in this,
-			//  as we need to process the stuff in list, right until we find a matching token
-			while (list.length) {
-				character = list.shift();
-
-				//  reduce provided escaping
-				if (character[character.length - 1] === '\\') {
-					if (!pattern.escape.test(list[0])) {
-						//  remove the escape character
-						character = character.substr(0, character.length - 1);
-					}
-
-					//  add the result
-					result.push(character);
-
-					//  while we are at it, we may aswel move the (at least previously) escaped
-					//  character to the result
-					result.push(list.shift());
-					continue;
-				}
-				else if (character === token) {
-					//  with the escaping taken care of, we now know the string has ended
-					break;
+		//  token is the initial (opening) quotation character, we are not (yet) interested in this,
+		//  as we need to process the stuff in list, right until we find a matching token
+		while (list.length) {
+			character = list.shift();
+console.log(character, character[character.length - 1]);
+			//  reduce provided escaping
+			if (character[character.length - 1] === '\\') {
+				if (!pattern.escape.test(list[0])) {
+					//  remove the escape character
+					character = character.substr(0, character.length - 1);
 				}
 
+				//  add the result
 				result.push(character);
+
+				//  while we are at it, we may aswel move the (at least previously) escaped
+				//  character to the result
+				result.push(list.shift());
+				continue;
+			}
+			else if (character === token) {
+				//  with the escaping taken care of, we now know the string has ended
+				break;
 			}
 
-			return addQuotation(result.join(''));
+			result.push(character);
 		}
 
-		/**
-		 *  Compile the JSON-formatted string from a list of 'tokenized' data
-		 *  @name    compiler
-		 *  @access  internal
-		 *  @param   Array   list
-		 *  @return  string  JSON-formatted
-		 */
-		function compiler(list) {
-			var result = '';
-
-			while (list.length) {
-				result = nibble(result, list);
-			}
-
-			return result;
-		}
-
-		/**
-		 *  Nibble the next token from the list and handle it
-		 *  @name    nibble
-		 *  @access  internal
-		 *  @param   string  result
-		 *  @param   array   tokens
-		 *  @return  string  result
-		 */
-		function nibble(result, list) {
-			var token = list.shift();
-
-			switch (token) {
-				//  ignore whitespace outside of quoted patterns
-				case ' ':
-					break;
-
-				//  remove any trailing commas and whitespace
-				case '}':
-				case ']':
-					result = removeTrailing(result) + token;
-					break;
-
-				//  add/remove escaping
-				case '"':
-				case '\'':
-					result += escapeQuotedInput(token, list);
-					break;
-
-				//  determine if the value needs to be quoted (always true if the next item in the list is a separator)
-				default:
-					result += addQuotation(token, list[0] === ':');
-					break;
-			}
-
-			return result;
-		}
-
-		/**
-		 *  Tokenize the input, adding each special character to be its own item in the resulting array
-		 *  @name    tokenize
-		 *  @access  internal
-		 *  @param   string  input
-		 *  @result  Array   tokens
-		 */
-		function tokenize(input) {
-			var result = [],
-				i;
-
-			//  check each character in the string
-			for (i = 0; i < input.length; ++i) {
-				//  if there is not result or the current or previous input is special, we create a new result item
-				if (result.length === 0 || isSpecial(input[i]) || isSpecial(result[result.length - 1])) {
-					result.push(input[i]);
-				}
-
-				//  extend the previous item
-				else {
-					result[result.length - 1] += input[i];
-				}
-			}
-
-			return result;
-		}
-
-		/**
-		 *  Apply Object or Array notation (string.replace helper for an expression resulting in ':' or ',')
-		 *  @name    notation
-		 *  @access  internal
-		 *  @param   string  full match
-		 *  @param   string  matching symbol
-		 *  @return  string  wrapped
-		 */
-		function notation(match, symbol) {
-			var character = symbol === ':' ? '{}' : '[]';
-
-			//  figure out if the notation should be added or may be skipped
-			return match[0] !== character[0] ? character[0] + removeTrailing(match) + character[1] : match;
-		}
-
-		/**
-		 *  Prepare a string to become a JSON-representation
-		 *  @name    prepare
-		 *  @access  public
-		 *  @param   string  input
-		 *  @return  string  JSON-formatted
-		 */
-		formatter.prepare = function(input) {
-			//  tokenize the input and feed it to the compiler in one go
-			return compiler(tokenize(input))
-				.replace(/^.*?([:,]).*$/, notation)
-			;
-		};
-
-		/**
-		 *  Prepare a string and parse it using JSON.parse
-		 *  @name    parse
-		 *  @access  public
-		 *  @param   string  input
-		 *  @return  mixed   parsed
-		 */
-		formatter.parse = function(input) {
-			return JSON.parse(formatter.prepare(input));
-		};
+		return addQuotation(result.join(''));
 	}
 
-	//  expose the interface
-	return JSONFormatter;
-});
+	/**
+	 *  Compile the JSON-formatted string from a list of 'tokenized' data
+	 *  @name    compiler
+	 *  @access  internal
+	 *  @param   Array   list
+	 *  @return  string  JSON-formatted
+	 */
+	function compiler(list) {
+		var result = '';
+
+		while (list.length) {
+			result = nibble(result, list);
+		}
+
+		return result;
+	}
+
+	/**
+	 *  Nibble the next token from the list and handle it
+	 *  @name    nibble
+	 *  @access  internal
+	 *  @param   string  result
+	 *  @param   array   tokens
+	 *  @return  string  result
+	 */
+	function nibble(result, list) {
+		var token = list.shift();
+
+		switch (token) {
+			//  ignore whitespace outside of quoted patterns
+			case ' ':
+				break;
+
+			//  remove any trailing commas and whitespace
+			case '}':
+			case ']':
+				result = removeTrailing(result) + token;
+				break;
+
+			//  add/remove escaping
+			case '"':
+			case '\'':
+				result += escapeQuotedInput(token, list);
+				break;
+
+			//  determine if the value needs to be quoted (always true if the next item in the list is a separator)
+			default:
+				result += addQuotation(token, list[0] === ':');
+				break;
+		}
+
+		return result;
+	}
+
+	/**
+	 *  Tokenize the input, adding each special character to be its own item in the resulting array
+	 *  @name    tokenize
+	 *  @access  internal
+	 *  @param   string  input
+	 *  @result  Array   tokens
+	 */
+	function tokenize(input) {
+		var result = [],
+			i;
+
+		//  check each character in the string
+		for (i = 0; i < input.length; ++i) {
+			//  if there is not result or the current or previous input is special, we create a new result item
+			if (result.length === 0 || isSpecial(input[i]) || isSpecial(result[result.length - 1])) {
+				result.push(input[i]);
+			}
+
+			//  extend the previous item
+			else {
+				result[result.length - 1] += input[i];
+			}
+		}
+
+		return result;
+	}
+
+	/**
+	 *  Apply Object or Array notation (string.replace helper for an expression resulting in ':' or ',')
+	 *  @name    notation
+	 *  @access  internal
+	 *  @param   string  full match
+	 *  @param   string  matching symbol
+	 *  @return  string  wrapped
+	 */
+	function notation(match, symbol) {
+		var character = symbol === ':' ? '{}' : '[]',
+			position = match.indexOf(symbol),
+			string = (match.match(/"/g) || []).length === 2 && match.indexOf('"') < position && match.lastIndexOf('"') > position;
+
+		//  figure out if the notation should be added or may be skipped
+		return !string && match[0] !== character[0] ? character[0] + removeTrailing(match) + character[1] : match;
+	}
+
+	/**
+	 *  Prepare a string to become a JSON-representation
+	 *  @name    prepare
+	 *  @access  public
+	 *  @param   string  input
+	 *  @return  string  JSON-formatted
+	 */
+	formatter.prepare = function(input) {
+		if (typeof input !== 'string') {
+			return '';
+		}
+
+		//  tokenize the input and feed it to the compiler in one go
+		return compiler(tokenize(input))
+			.replace(/^.*?([:,]).*$/, notation)
+		;
+	};
+
+	/**
+	 *  Prepare a string and parse it using JSON.parse
+	 *  @name    parse
+	 *  @access  public
+	 *  @param   string  input
+	 *  @return  mixed   parsed
+	 */
+	formatter.parse = function(input) {
+		var prepared = formatter.prepare(input);
+
+		return prepared ? JSON.parse(prepared) : null;
+	};
+}
---
+++ test/lib/json-formatter.js
@@ -0,0 +1,109 @@
+/*global JSONFormatter, describe, it, expect*/
+describe('JSONFormatter', function() {
+	'use strict';
+
+	it('is a singleton', function() {
+		expect(JSONFormatter()).toBe(JSONFormatter());
+		expect(JSONFormatter()).toBe(new JSONFormatter());
+		expect(new JSONFormatter()).toBe(JSONFormatter());
+		expect(new JSONFormatter()).toBe(new JSONFormatter());
+	});
+
+	it('ignores non-string values', function() {
+		var json = new JSONFormatter();
+
+		[1, Math.PI, true, false, [], {}, [1,'a'], {a:1}].forEach(function(v) {
+			expect(json.prepare(v)).toBe('');
+			expect(json.parse(v)).toBe(null);
+		});
+	});
+
+	it('parses proper JSON syntax', function() {
+		var json = new JSONFormatter();
+
+		expect(json.parse('hello')).toBe('hello');
+
+		expect(json.parse('true')).toBe(true);
+		expect(json.parse('false')).toBe(false);
+
+		expect(json.parse('123')).toBe(123);
+		expect(json.parse('123.45')).toBe(123.45);
+
+		expect(json.parse('[1, 2, 3]')).toEqual([1, 2, 3]);
+
+		expect(json.parse('{"int":1, "float": -1.23, "boolfalse": false, "booltrue": true, "string": "string", "array":[1, 2]}')).toEqual({
+			int: 1,
+			float: -1.23,
+			boolfalse: false,
+			booltrue: true,
+			string: 'string',
+			array: [1, 2]
+		});
+	});
+
+	it('is relaxed about the outer wrapping', function() {
+		var json = new JSONFormatter();
+
+		expect(json.parse('hello, world')).toEqual(['hello', 'world']);
+		expect(json.parse('hello: world')).toEqual({hello:'world'});
+		expect(json.parse('foo: bar, baz: true')).toEqual({foo:'bar', baz:true});
+	});
+
+	it('is relaxed about trailing commas', function() {
+		var json = new JSONFormatter();
+
+		expect(json.prepare('1, 2,')).toBe('[1,2]');
+		expect(json.parse('3, 4,')).toEqual([3,4]);
+
+		expect(json.prepare('hello: world,')).toBe('{"hello":"world"}');
+		expect(json.parse('hello: world,')).toEqual({hello:'world'});
+	});
+
+	it('respects quoted values', function() {
+		var json = new JSONFormatter();
+
+		expect(json.prepare('"hello:world"')).toBe('"hello:world"');
+		expect(json.parse('"hello:world"')).toBe('hello:world');
+
+		expect(json.prepare('"hello,world"')).toBe('"hello,world"');
+		expect(json.parse('"hello,world"')).toBe('hello,world');
+
+		expect(json.prepare('"hello,\'world"')).toBe('"hello,\'world"');
+		expect(json.parse('"hello,\'world"')).toBe('hello,\'world');
+
+		expect(json.prepare('"hello,\'world\'"')).toBe('"hello,\'world\'"');
+		expect(json.parse('"hello,\'world\'"')).toBe('hello,\'world\'');
+	});
+
+	it('trims keys and values', function() {
+		var json = new JSONFormatter();
+
+		expect(json.parse('    hello    ')).toEqual('hello');
+		expect(json.parse('    hello    :    world    ')).toEqual({hello:'world'});
+	});
+
+	it('optimize escape characters', function() {
+		var json = new JSONFormatter();
+
+		expect(json.prepare('hello\\"world')).toBe('hello\\"world');
+	});
+
+	it('allows keywords and numbers to be used as object keys', function() {
+		var json = new JSONFormatter();
+
+		expect(json.prepare('true:true')).toBe('{"true":true}');
+		expect(json.parse('true:true')).toEqual({true:true});
+
+		expect(json.prepare('false:false')).toBe('{"false":false}');
+		expect(json.parse('false:false')).toEqual({false:false});
+
+		expect(json.prepare('null:null')).toBe('{"null":null}');
+		expect(json.parse('null:null')).toEqual({null:null});
+
+		expect(json.prepare('123:456')).toBe('{"123":456}');
+		expect(json.parse('123:456')).toEqual({123:456});
+
+		expect(json.prepare('true: true, false: false')).toBe('{"true":true,"false":false}');
+		expect(json.parse('true: true, false: false')).toEqual({true:true,false:false});
+	});
+});
