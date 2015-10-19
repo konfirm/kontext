5f6c3ed273b7761ff10c4593dafda318edba553a
Rogier Spieker <rogier@konfirm.net>
2015-10-19 12:32:13 +0200

    Added test for implicit delegates

--- test/kontext/delegate.js
+++ test/kontext/delegate.js
@@ -16,7 +16,34 @@ describe('Kontext Delegate', function() {
 		done();
 	});
 
-	it('does not meddle with previously created delegates', function(done) {
+	it('does not meddle with implicitly created delegates', function(done) {
+		var model = {
+				foo: 'bar'
+			},
+			a, b, desc;
+
+		a = kontext.bind(model, document.body);
+
+		desc = Object.getOwnPropertyDescriptor(a, 'foo');
+		expect(typeof desc).toBe('object');
+		expect(typeof desc.get).toBe('function');
+		expect(typeof desc.set).toBe('function');
+		expect(desc.get).toBe(desc.set);
+
+		b = kontext.bind(a, document.body);
+
+		desc = Object.getOwnPropertyDescriptor(b, 'foo');
+		expect(typeof desc).toBe('object');
+		expect(typeof desc.get).toBe('function');
+		expect(typeof desc.set).toBe('function');
+		expect(desc.get).toBe(desc.set);
+
+		expect(a).toBe(b);
+
+		done();
+	});
+
+	it('does not meddle with explicitly created delegates', function(done) {
 		var model = {
 				foo: 'bar'
 			},
